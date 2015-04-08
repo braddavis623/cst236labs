dac83cbfba8088f111ec4c4e1d2ee07f21f8a6fe
Brad Davis <brads_e_mail@hotmail.com>
2015-04-08 11:47:34 -0700

    added rhombus function and its tests

--- source/source1.py
+++ source/source1.py
@@ -47,3 +47,57 @@ def get_triangle_type(a=0, b=0, c=0):
         return "isosceles"
     else:
         return "scalene"
+
+
+def get_rhombus_type(a=0, b=0, c=0, d=0):
+    """
+    Determine if the given dimensions/angles constitute a square, rectangle,
+    rhombus, or no polygon
+
+    :param a: line a
+    :type a: float or int or tuple or list or dict
+
+    :param b: line b
+    :type b: float
+
+    :param c: line c
+    :type c: float
+
+    :param d: line d
+    :type d: float
+
+
+    :return: "square", "rectangle", "rhombus", "disconnected object" or "invalid"
+    :rtype: str
+    """
+    if isinstance(a, (tuple, list)) and len(a) == 4:
+        d = a[3]
+        c = a[2]
+        b = a[1]
+        a = a[0]
+
+    if isinstance(a, dict) and len(a.keys()) == 4:
+        values = []
+        for value in a.values():
+            values.append(value)
+        a = values[0]
+        b = values[1]
+        c = values[2]
+        d = values[3]
+
+    if not (isinstance(a, (int, float)) and isinstance(b, (int, float)) and isinstance(c, (int, float)) and isinstance(d,(int, float))):
+        return "invalid"
+
+    if a <= 0 or b <= 0 or c <= 0 or d <= 0:
+        return "invalid"
+
+    if a == b and b == c and a == d:
+        return "square"
+
+    elif (a + b == c + d or a + c == b + d) and a + b + c + d != 360:
+        return "rectangle"
+
+    elif a + b + c + d == 360:
+        return "rhombus"
+    else:
+        return "disconnected object"
--- tests/source1_test.py
+++ tests/source1_test.py
@@ -2,7 +2,9 @@
 Test for source.source1
 """
 from source.source1 import get_triangle_type
+from source.source1 import get_rhombus_type
 from unittest import TestCase
+
 
 class TestGetTriangleType(TestCase):
 
@@ -28,14 +30,47 @@ class TestGetTriangleType(TestCase):
         result = get_triangle_type(1, 1, 2)
         self.assertEqual(result, 'isosceles')
 
-    def test_get_triangle_scalene_all_int(self):        # provided
-        result = get_triangle_type(1, 2, 3)
-        self.assertEqual(result, 'scalene')
-
     def test_get_triangle_improper_type(self):          # added for lab 1
         result = get_triangle_type(1, "a", 3)
         self.assertEqual(result, 'invalid')
 
     def test_get_triangle_negative_value(self):          # added for lab 1
         result = get_triangle_type(1, -1, 3)
+        self.assertEqual(result, 'invalid')
+
+
+class TestGetRhombusType(TestCase):
+
+    def test_get_rhombus_square_all_int(self):    # added for lab 1
+        result = get_rhombus_type(1, 1, 1, 1)
+        self.assertEqual(result, 'square')
+
+    def test_get_rhombus_square_tuple(self):      # added for lab 1
+        tu = (2, 2, 2, 2)
+        result = get_rhombus_type(tu)
+        self.assertEqual(result, 'square')
+
+    def test_get_rhombus_square_dict(self):      # added for lab 1
+        dic = {'a': 2, 'b': 2, 'c': 2, 'd': 2}
+        result = get_rhombus_type(dic)
+        self.assertEqual(result, 'square')
+
+    def test_get_rhombus_rectangle_all_int(self):        # added for lab 1
+        result = get_rhombus_type(1, 2, 1, 2)
+        self.assertEqual(result, 'rectangle')
+
+    def test_get_rhombus_rhombus_all_int(self):      # added for lab 1
+        result = get_rhombus_type(60, 120, 60, 120)
+        self.assertEqual(result, 'rhombus')
+
+    def test_get_rhombus_disconnected_all_int(self):        # added for lab 1
+        result = get_rhombus_type(60, 120, 60, 200)
+        self.assertEqual(result, 'disconnected object')
+
+    def test_get_rhombus_improper_type(self):          # added for lab 1
+        result = get_rhombus_type(1, "a", 3, 4)
+        self.assertEqual(result, 'invalid')
+
+    def test_get_rhombus_negative_value(self):          # added for lab 1
+        result = get_rhombus_type(1, -1, 3, 3)
         self.assertEqual(result, 'invalid')

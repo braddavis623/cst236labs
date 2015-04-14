ddff4a0fa992b787f7d2d685bc7cfd3646cba858
Brad Davis <brads_e_mail@hotmail.com>
2015-04-13 19:48:28 -0700

    another attempt at updating reqs.txt

---
+++ tests/lab2round1orc_test.py
@@ -0,0 +1,12 @@
+"""
+test for source.lab2round1orc.py
+"""
+
+from unittest import TestCase
+import logging
+
+logger = logging.getLogger(__name__)
+loging.getLogger("z.fileA").setLevel(__debug__)
+class TestOrcTest (TestCase):
+
+    def Test_Orc(self):
